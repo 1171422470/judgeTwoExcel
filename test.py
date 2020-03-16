4e5be1f98f7e12d8d87ca4d30ec600fab22935c5
MIKE CAO <41140968+1171422470@users.noreply.github.com>
2020-03-16 16:49:48 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-
-a = [[1,2,3],[4,5,6],['a','b']]
-b = [[1,2,4],[4,8,6],['c',3]]
-c = []
-for i in range(len(a)) :
-    for j in range(len(a[i])):
-        if a[i][j] != b[i][j]:
-            print(i,j)
