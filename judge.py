7220f8ed6f8c2f6d0f4a1c6797f999856b9ffd11
MIKE CAO <1171422470@qq.com>
2020-03-17 11:08:41 +0800

    3.16上传

--- judge.py
+++ judge.py
@@ -4,6 +4,8 @@
 #日期:20200316
 """
 import xlrd
+import xlwt
+from xlutils.copy import copy
 
 class Data_Excel():
     def __init__(self,file_add1,file_add2):
@@ -25,7 +27,6 @@ class Data_Excel():
         table1 = data1.sheets()[0]       #获取sheet表
         table2 = data2.sheets()[0]  # 获取sheet表
 
-
         for i in range(0,table1.nrows):
             value = table1.row_values(i, start_colx=0, end_colx=None)
             self.list1.append(value)
@@ -37,23 +38,40 @@ class Data_Excel():
         return self.list1,self.list2
 
     def is_same(self,list1,list2):
+        workbooknew = copy(self.data1)
+        ws = workbooknew.get_sheet(0)
+
+        #创建颜色样式-----------
+        pattern = xlwt.Pattern()
+        pattern.pattern = xlwt.Pattern.SOLID_PATTERN
+        pattern.pattern_fore_colour = 2
+        style = xlwt.XFStyle()
+        style.pattern = pattern
+        #---------------------
         if len(list1) != 0 and len(list2) != 0:
             flag = 0
-            table1 = data1.sheets()[0]  # 获取sheet表
-            table2 = data2.sheets()[0]  # 获取sheet表
+            table1 = self.data1.sheets()[0]  # 获取sheet表
+            table2 = self.data2.sheets()[0]  # 获取sheet表
 
             if table1.nrows == table2.nrows and table1.ncols == table2.ncols:
                 for i in range(len(list1)):
                    for j in range(len(list1[i])):
                        if list1[i][j] != list2[i][j]:
                            flag = 1
-                           print(i,j)
+                           print("行：", i, "列:", j)
+                           ws.write(i, j, list1[i][j], style)
+                           try:
+                               workbooknew.save(r'C:\Users\10003144\Desktop\test2.xlsx')
+                           except Exception:
+                               print("Excel文件请先关闭!")
+
                 if flag == 0:
-                    print("两个Excel一致!")
+                    return "两个Excel一致"         #将不一致的行列标记，并标红
             else:
-                print("两个Excel不一致!")
+                list3 = [[table1.nrows,table1.ncols],[table2.nrows,table2.ncols]]
+                return list3                      #将两个工作簿的行列标记出来
         else:
-            print("存在空工作簿")
+            return self.file_add1,self.file_add2  #打印两个工作簿的地址-存在空工作簿
         return
 
 if __name__ == '__main__':
@@ -63,4 +81,5 @@ if __name__ == '__main__':
     data = Data_Excel(file1,file2)
     data1,data2=data.open_excel()
     list1,list2 = data.operate_excel(data1,data2)
-    data.is_same(list1,list2)
+    val = data.is_same(list1,list2)
+    print(val)
